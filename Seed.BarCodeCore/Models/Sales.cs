fd17ea1bada5b2f09b481f4d382ef770759a3331
NoZero <3921342@qq.com>
2016-10-28 16:03:44 +0800

    change entities

--- Seed.BarCodeCore/Models/Sales.cs
+++ Seed.BarCodeCore/Models/Sales.cs
@@ -7,11 +7,17 @@ using Seed.BarCodeCore.Interface;
 
 namespace Seed.BarCodeCore.Models
 {
-    public class Store:IId
+    public class Sale:IId
     {
         public int Id { get; set; }
         public string OrderInfo { get; set; }
-        public string BigCode { get; set; }
+
+        public string SaleInfo { get; set; }
         public DateTime CreateTime { get; set; }
+
+         public Sale()
+        {
+            CreateTime = DateTime.Now;
+        }
     }
 }
